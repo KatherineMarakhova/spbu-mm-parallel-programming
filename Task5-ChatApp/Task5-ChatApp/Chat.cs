c83102b95855ff8762fd631767f4ff6cf1680dc4
Katherine Marakhova <40491346+KatherineMarakhova@users.noreply.github.com>
2024-06-01 10:49:15 +0300

    Added Task5

---
+++ Task5-ChatApp/Task5-ChatApp/Chat.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ChatApp
+{
+
+    public partial class Chat
+    {
+        private delegate void SafeCallDelegate(string text);
+        private Socket BroadcastSendSocket;
+        private Socket BroadcastRecvSocket;
+        private IPEndPoint RemoteEndPoint = new IPEndPoint(new IPAddress(new byte[] {255, 255, 255, 255}), 1000);
+        private Thread Listener;
+
+        public void Connect(int userId, CancellationToken token)
+        {
+            try
+            {
+                BroadcastSendSocket = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Dgram, ProtocolType.Udp);
+                BroadcastSendSocket.SetSocketOption(SocketOptionLevel.Socket,
+                    SocketOptionName.Broadcast, true);
+                BroadcastSendSocket.SetSocketOption(SocketOptionLevel.Socket,
+                    SocketOptionName.ReuseAddress, true);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message + '\n');
+                return;
+            }
+            Send(userId, "was conneted!\n");
+
+            Listener = new Thread(() => Listen(token));
+            Listener.Start();
+        }
+
+        private void Listen(CancellationToken token)
+        {
+            try
+            {
+                BroadcastRecvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                BroadcastRecvSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+                BroadcastRecvSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                var broadcastEndpoint = new IPEndPoint(IPAddress.Any, 1000);
+                BroadcastRecvSocket.Bind(broadcastEndpoint);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message + " - Error with socket creation\n");
+                return;
+            }
+            while (!token.IsCancellationRequested)
+            {
+                if (BroadcastRecvSocket.Available != 0)
+                {
+                    try
+                    {
+                        byte[] b = new byte[1024];
+                        BroadcastRecvSocket.Receive(b);
+                        Console.WriteLine(Encoding.ASCII.GetString(b, 0, Array.IndexOf(b, (byte)0)) + '\n');
+                    }
+                    catch (SocketException se)
+                    {
+                        Console.WriteLine(se.Message + " - Error with receiving\n");
+                        return;
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        return;
+                    }
+                }
+                Thread.Yield();
+            }
+        }
+
+        public void Send(int userId, string message)
+        {
+            try
+            {
+                byte[] byteMessage = Encoding.ASCII.GetBytes(userId + " : " + message + '\n');
+                BroadcastSendSocket.SendTo(byteMessage, RemoteEndPoint);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message + " - Error with sending message\n");
+                return;
+            }
+        }
+    }
+}
---
+++ Task5-ChatApp/Task5-ChatApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ChatApp
+{
+
+    public partial class Program
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("\n" +
+                              "This program generate p2p chat.      \n " +
+                              "For connecting to the chat press 'C' \n ");
+
+            Char key = Console.ReadKey().KeyChar;
+            Console.WriteLine("\n");
+
+            if (key == 'C' || key == 'c')
+            {
+                Chat chat = new Chat();
+                CancellationTokenSource TokenSource = new CancellationTokenSource();
+                CancellationToken myToken = TokenSource.Token;
+
+                Random randomID = new Random();
+                int userId = randomID.Next(1000, 9999);
+
+                chat.Connect(userId, TokenSource.Token);
+
+                Console.WriteLine("\n Welcome! \n " +
+                                  "Your ID is " + userId +     "\n" +
+                                  "At any time to write a message press 'M' \n " +
+                                  "Press 'Q' to quit the dialog \n "); ;
+                
+
+                while (!myToken.IsCancellationRequested)
+                {
+                    Char keyMessage;
+                    keyMessage = Char.ToUpper(Console.ReadKey().KeyChar);
+
+                    switch (keyMessage)
+                    {
+                        case 'M':
+                            Console.WriteLine("\n" + userId + " : ");
+                            String message = Console.ReadLine();
+
+                            if (message != "")
+                                chat.Send(userId, message);
+                            Console.WriteLine("\n");
+                            break;
+
+                        case 'Q':
+                            TokenSource.Cancel();
+                            break;
+
+                        default:
+                            Console.WriteLine("\n" +
+                              "Unknown key-message.          \n " +
+                              "Press 'M' to wrire a message. \n " +
+                              "Press 'Q' to quit.            \n ");
+                            break;
+
+                    }
+                }
+                TokenSource.Dispose();
+            }
+            Console.ReadKey();
+        }
+    }
+}
